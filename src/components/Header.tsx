349ac92f724d7f72263cdc18fcfd9caaee073cd7
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-01-12 06:55:55 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   const activeStyles: ActiveStyles = {
     fontSize: "1.15em",
     fontWeight: "600",
-    textShadow: "1px 1px 1px lightgray",
+    textShadow: ".7px .7px .7px lightgray",
     color:"#266ea3"
   };
   const pendingStyles: PendingStyles = {
