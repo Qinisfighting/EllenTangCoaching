ee30b1f11db3b4915a40e451f9f1f10900277626
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-01-13 17:39:18 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import profil from "../assets/profil.png";
 
 
 export default function Home() {
     return (
-      <div className="flex flex-col p-4 w-11/12 md:w-4/5 mx-auto my-16 lg:flex-row gap-8 justify-between text-myblue-800">
+      <div className="flex flex-col px-4 w-11/12 md:w-4/5 mx-auto my-16 lg:flex-row gap-8 justify-between text-myblue-800">
          <img src={profil} alt="me" className="w-full h-auto lg:w-1/3 object-cover shadow-lg"/>
          <div className="flex flex-col justify-between items-end gap-16">
             <div className="text-center text-xl">
@@ -13,7 +13,7 @@ export default function Home() {
               <p className="">I collaborate with individuals all over the world who are on their paths towards self-discovery, growth, and fulfilment. Together, we will co-create an environment where unconditional acceptance of ourselves is the norm, because it is only then can we clear our clouded visions and explore freely what a fulfilled life looks like for each one of us as unique individuals.</p>
               <p className="">I cannot wait to start this thrilling journey with you.</p>
             </div>
-            <button className="btn-next">GET TO KNOW ME</button>
+            <Link to="/about"><button className="btn-next">GET TO KNOW ME</button></Link>
          </div>
       </div>
     );
