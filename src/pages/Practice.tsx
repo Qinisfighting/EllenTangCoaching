17e10124c988397973a0b6ce71e089fdd7a401b1
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-01-13 09:30:49 +0100

    pactice

--- src/pages/Practice.tsx
+++ src/pages/Practice.tsx
@@ -1,40 +1,90 @@
 export default function Practice() {
     return (
-      <div className="mx-auto my-20 px-6 text-justify w-11/12 text-curiousBlue-900">
-        <h1>Privacy Policy</h1>
-        <p>Ellen Lei Tang (“Ellen Tang”) operates ellentangcoaching.com and may operate other websites. It is Ellen Tang’s policy to respect your privacy regarding any information we may collect while operating our websites.</p>
-        <h3>Website Visitors</h3>
+      <div className="mx-auto my-20 px-6 w-11/12 text-curiousBlue-900">
+        <h1>Practice Policy</h1>
+        
+        <h3>Types of personal development service available:</h3>
         <p>
-        Like most website operators, Ellen Tang collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Ellen Tang’s purpose in collecting non-personally identifying information is to better understand how Ellen Tang’s visitors use its website. From time to time, Ellen Tang may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.
+        Clients are defined as responsible adults not suffering from severe mental health conditions. My services include: 
         </p> 
+        <ul>
+          <li>- Personal life coaching</li>
+          <li>- Professional life coaching</li>
+          <li>- Past trauma coaching</li>
+          <li>- Confidence coaching</li>
+          <li>- Self-esteem coaching</li>
+          <li>- Emotional intelligence coaching</li>
+          <li>- Depression/Anxiety coaching</li>
+          <li>- Money mindset coaching</li>
+          <li>- Career development coaching</li>
+          <li>- Career change coaching</li>
+          <li>- Self-awareness coaching</li>
+          <li>- Relationship coaching</li>
+        </ul>
+        
+        <h3>Sessions</h3>
         <p>
-        Ellen Tang also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on ellentangcoaching.com blogs/sites. Ellen Tang only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below, except that commenter IP addresses and email addresses are visible and disclosed to the administrators of the blog/site where the comment was left.
+        Regular times for sessions are agreed at the start of the working engagement and remain yours unless changed by
+        mutual agreement. Sessions begin at ten minutes past the hour and end on the hour, giving time for transitions
+        between sessions. Sessions end on the hour even if they start late unless that lateness is Ellen’s fault. 
         </p>
-        <h3>Gathering of Personally-Identifying Information</h3>
+        <ul>
+          <li>- A full session is of 50 minutes duration. </li>
+        </ul>
         <p>
-        Certain visitors to Ellen Tang’s websites choose to interact with Ellen Tang in ways that require Ellen Tang to gather personally-identifying information. The amount and type of information that Ellen Tang gathers depends on the nature of the interaction. For example, we ask visitors who sign up at http://www.ellentangcoaching.com to provide a username and email address. Those who engage in transactions with Ellen Tang are asked to provide additional information, including as necessary the personal and financial information required to process those transactions. In each case, Ellen Tang collects such information only insofar as is necessary or appropriate to fulfill the purpose of the visitor’s interaction with Ellen Tang. Ellen Tang does not disclose personally-identifying information other than as described below. And visitors can always refuse to supply personally-identifying information, with the caveat that it may prevent them from engaging in certain website-related activities.
+        All sessions can be extended if time is available. When a session is extended the fee is adjusted on a pro rata basis. 
         </p>
-        <h3>Aggregated Statistics</h3>
+
+        <h3>Emergency calls</h3>
         <p>
-        Ellen Tang may collect statistics about the behavior of visitors to its websites. Ellen Tang may display this information publicly or provide it to others. However, Ellen Tang does not disclose personally-identifying information other than as described below.
+        You may telephone or email at times other than your regular hours if you need to. Reasonable time spent on such
+        calls will not be charged for. I discourage texting for practical reasons related to immediacy and confidentiality. 
         </p>
-        <h3>Protection of Certain Personally-Identifying Information</h3>
+        <h3>Payment</h3>
         <p>
-        Ellen Tang discloses potentially personally-identifying and personally-identifying information only to those of its employees, contractors and affiliated organizations that (i) need to know that information in order to process it on Ellen Tang’s behalf or to provide services available at Ellen Tang’s websites, and (ii) that have agreed not to disclose it to others. Some of those employees, contractors and affiliated organizations may be located outside of your home country; by using Ellen Tang’s websites, you consent to the transfer of such information to them. Ellen Tang will not rent or sell potentially personally-identifying and personally-identifying information to anyone. Other than to its employees, contractors and affiliated organizations, as described above, Ellen Tang discloses potentially personally-identifying and personally-identifying information only in response to a subpoena, court order or other governmental request, or when Ellen Tang believes in good faith that disclosure is reasonably necessary to protect the property or rights of Ellen Tang, third parties or the public at large. If you are a registered user of an Ellen Tang website and have supplied your email address, Ellen Tang may occasionally send you an email to tell you about new features, solicit your feedback, or just keep you up to date with what’s going on with Ellen Tang and our products. If you send us a request (for example via email or via one of our feedback mechanisms), we reserve the right to publish it in order to help us clarify or respond to your request or to help us support other users. Ellen Tang takes all measures reasonably necessary to protect against the unauthorized access, use, alteration or destruction of potentially personally-identifying and personally-identifying information.
+        Fees are payable in full before each session unless otherwise agreed. They may be made in cash, via bank transfer, or
+by debit/credit card. 
         </p>
-        <h3>Cookies</h3>   
+        <h3>Fees (2021): </h3>  
+        <ul>
+          <li>- Individual sessions: GBP 90.00</li>
+        </ul> 
         <p>
-        A cookie is a string of information that a website stores on a visitor’s computer, and that the visitor’s browser provides to the website each time the visitor returns. Ellen Tang uses cookies to help Ellen Tang identify and track visitors, their usage of Ellen Tang website, and their website access preferences. Ellen Tang visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Ellen Tang’s websites, with the drawback that certain features of Ellen Tang’s websites may not function properly without the aid of cookies.
+        Fees are increased once a year in line with inflation. 
         </p>
-        <h3>Business Transfers</h3>
+        <h3>Cancellation policy</h3>
         <p>
-        If Ellen Tang, or substantially all of its assets, were acquired, or in the unlikely event that Ellen Tang goes out of business or enters bankruptcy, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any acquirer of Ellen Tang may continue to use your personal information as set forth in this policy.
+          Sessions may be cancelled or rescheduled by the client without charge provided that a minimum of 24 hours’ notice
+        is given. Cancellation or rescheduling within 24 hours or less before a booked session will be charged the full fee. In
+        the event of a late cancellation or rescheduling due to “Acts of God” a half fee is payable. 
         </p>
-        <h3>Ads</h3>
+        <h3>Confidentiality</h3>
         <p>
-        Although most changes are likely to be minor, Ellen Tang may change its Privacy Policy from time to time, and in Ellen Tang’s sole discretion. Ellen Tang encourages visitors to frequently check this page for any changes to its Privacy Policy. If you have a ellentangcoaching.com account, you might also receive an alert informing you of these changes. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.
+        Anything you say or do during sessions is held in complete confidence and will not be repeated without your explicit
+permission. Clients are not bound by such confidentiality and may discuss their work with whom they choose.
+Partners form their own agreements regarding the confidentiality of each other’s material. I keep no notes of
+sessions. This is for reasons of good practice and to maximise client confidentiality. If you have specific concerns
+around confidentiality related perhaps to your social circumstances or the nature of your work, please be sure to
+raise them so we can ensure safe working. 
         </p>
+        <h3>Online working considerations</h3>
+        <p>
+        Working online offers a range of opportunities not available when working in person. These include convenience, a
+high level of confidentiality and the ability to physically reveal aspects of one’s life that a client might otherwise
+prefer to keep concealed. It is the responsibility of the client to ensure that his or her use of online coaching
+conforms to the relevant legislation in her or his country. I am highly attentive to the online technology in use, but
+technological quality cannot be guaranteed.
+        </p>
+      <address className="text-curiousBlue-500 text-lg px-6 my-6 border-l"> 
+      <p className="font-semibold">Ellen Tang Coaching <br />
+      Email: <a className="underline"  href="mailto:ellen@ellentangcoaching.com">ellen@ellentangcoaching.com</a><br />
+      Tel: (UK) +44 7753137614 / (US) +1 917-982-2022
       
+      
+      </p>
+      
+        
+      </address>
       
       </div>
     );
