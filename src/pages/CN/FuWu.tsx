681c6a62278d9b2da63145beb010a9f0e139001d
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-01-22 21:11:55 +0100

    Update FuWu.tsx

--- src/pages/CN/FuWu.tsx
+++ src/pages/CN/FuWu.tsx
@@ -32,7 +32,7 @@ export default function FuWu() {
              <p className="text-xl">语言：英文，中文（普通话）</p>
              <p className="text-xl">预约时间:（英国伦敦时间）周一至周五 09:00 – 19:00。如果你需要在这之外的时间咨询的话，请<Link to="/cn/yuyue" className="underline text-myblue-400">与我联系</Link>。</p>
            </div>  
-           <Link to="/cn/wenti"><button className="btn-next  my-12 lg:mt-16 text-md w-40">常见问题 ➣</button></Link>
+           <Link to="/cn/wenti"><button className="btn-next  mt-12 mb-2 lg:mt-16 text-md w-40">常见问题 ➣</button></Link>
         </div>
         <h1 className="text-4xl text-myblue-400 border-y w-4/5 my-12 md:w-1/3 md:mt-8 mx-auto">收费标准</h1>
         <div className="mx-auto w-5/6 my-21">
@@ -67,7 +67,7 @@ export default function FuWu() {
                   <p className='italic'>£150*</p>
                 </div>
                 <p className='border border-myblue-100 text-xs px-4'>*包含场地租借费用</p> 
-                <Link to="/cn/yuyue"><button className="btn-next  my-12 lg:mt-16 text-md w-40">预约咨询 ➣</button></Link>          
+                <Link to="/cn/yuyue"><button className="btn-next  mt-12 mb-2 lg:mt-16 text-md w-40">预约咨询 ➣</button></Link>          
               </div> 
            </div>                      
         </div>
@@ -86,7 +86,7 @@ export default function FuWu() {
                   <p className='italic'>£600*</p>
                 </div>
                 <p className='border border-myblue-100 text-xs px-4'>*包含场地租借费用</p> 
-                <Link to="/cn/yuyue"><button className="btn-next  my-12 lg:mt-16 text-md w-40">预约咨询 ➣</button></Link>     
+                <Link to="/cn/yuyue"><button className="btn-next mt-12 mb-2 lg:mt-16 text-md w-40">预约咨询 ➣</button></Link>     
           </div> 
                                 
         </div>
