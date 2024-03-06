0a9ad52b0e7c0ba674f5814ef746b3b3697cac8b
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-03-06 08:35:02 +0100

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,10 +3,8 @@ import loader from '../assets/loader.svg';
 
 export default function Loader() {
     return (
-        <div className="absolute z-50 h-full w-full flex items-start justify-center pt-20">
-        <div className="flex items-start justify-center">
-          <img src={loader} alt="loader" className="w-24 h-24" />
-        </div>
+      <div className="absolute z-50 h-full w-full flex items-start justify-center pt-20">    
+          <img src={loader} alt="loader" className="w-20 h-20 md:w-24 md:h-24" /> 
       </div>
     )
 }
