8e074025f03b3010df4886b2dced11845d87775e
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-03-23 20:56:22 +0100

    hide firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,15 +6,19 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 import { getAuth } from 'firebase/auth';
 
+// require('dotenv').config();
+
+
+
 // import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCufE7Vck3qF6rGbBEilBVsBwwYOIvsmqs",
-  authDomain: "ellen-tang-coaching.firebaseapp.com",
-  projectId: "ellen-tang-coaching",
-  storageBucket: "ellen-tang-coaching.appspot.com",
-  messagingSenderId: "820521381752",
-  appId: "1:820521381752:web:5978faa7a2aaa1a1a3228d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
